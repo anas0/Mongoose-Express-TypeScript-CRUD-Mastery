e6f4d5a33bb0b36cbff7cecb99b5f69c58dc6f0b
anas0 <ashraful0a@gmail.com>
2023-12-07 13:14:50 +0600

    some correction

--- src/modules/user/user.model.ts
+++ src/modules/user/user.model.ts
@@ -4,83 +4,65 @@ import { IAddress, IFullName, IOrder, IUser } from './user.interface';
 const fullNameSchema = new Schema<IFullName>({
   firstName: {
     type: String,
-    required: [true, 'Please tell us your firstName'],
   },
   lastName: {
     type: String,
-    required: [true, 'Please tell us your lastName'],
   },
 });
 
 const addressSchema = new Schema<IAddress>({
   street: {
     type: String,
-    required: [true, 'Please tell us your street'],
   },
   city: {
     type: String,
-    required: [true, 'Please tell us your city'],
   },
   country: {
     type: String,
-    required: [true, 'Please tell us your country'],
   },
 });
 
 const orderSchema = new Schema<IOrder>({
   productName: {
     type: String,
-    required: [true, 'Please tell us your productName'],
   },
   price: {
     type: Number,
-    required: [true, 'Please tell us your price'],
   },
   quantity: {
     type: Number,
-    required: [true, 'Please tell us your quantity'],
   },
 });
 
 const userSchema = new Schema<IUser>({
   userId: {
     type: Number,
-    required: [true, 'Please tell us your userId'],
     unique: true,
   },
   username: {
     type: String,
-    required: [true, 'Please tell us your username'],
     unique: true,
   },
   password: {
     type: String,
-    required: [true, 'Please tell us your password'],
-    select: false,
   },
   fullName: {
     type: fullNameSchema,
-    required: [true, 'fullName is required'],
   },
   age: {
     type: Number,
-    required: [true, 'Please tell us your age'],
   },
   email: {
     type: String,
-    required: [true, 'Please tell us your email'],
   },
   isActive: {
     type: Boolean,
-    required: [true, 'Please tell us your isActive status'],
   },
   hobbies: {
     type: [String],
-    required: [true, 'Please tell us your hobbies'],
   },
   address: {
     type: addressSchema,
-    required: [true, 'address is required'],
   },
   orders: {
     type: [orderSchema],
